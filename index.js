b1d6b26c70e5d691f901efaec44b465496ebb58c
Shokhjahon Alijonov <alijonovshohjahon49@gmail.com>
2026-06-10 21:34:35 +0500

    added block users route

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 const app = require('./app');
 const { errorHandler } = require('./middleware/error');
+const checkAuthentication = require('./middleware/authChecker');
 const PORT = process.env.PORT || 3000;
 
-const authRouter = require("./routes/auth");
+const authRouter = require('./routes/auth');
+const userRouter = require('./routes/users');
 
 app.use('/api/auth', authRouter);
+app.use('/api/users', checkAuthentication, userRouter);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
